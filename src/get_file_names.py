5b1231efcf5cb0090b0d9811e09677cd9225448b
Himel Das <himelhub@gmail.com>
2020-06-04 09:20:07 +0600

    refactor

--- src/get_file_names.py
+++ src/get_file_names.py
@@ -1,5 +1,19 @@
+"""
+A python script to show all the file names inside the directory recursively,
+as well as get it in the clipboard.
+
+Usage:
+python get_file_names.py directory_path
+
+If no argument is passed, it will consider the present directory where the
+app is located.
+"""
+
+
 import os
 import sys
+import pyperclip
+
 
 script_name = __file__.split('\\')[-1:][0]
 
@@ -11,12 +25,23 @@ else:
 
 
 def main():
+    files = []
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
             if file != script_name:
-                file_name = os.path.join(r, file)
-                print(file)
+                # file_name = os.path.join(r, file)
+                files.append(file)
+
+    # Writing all file names into a string to paste in the clipboard
+    string = ''
+    for file in files:
+        string = string + file + '\n'
+
+    pyperclip.copy(string)
+    pyperclip.paste()
+
+    print(string)
 
 
 if __name__ == '__main__':
