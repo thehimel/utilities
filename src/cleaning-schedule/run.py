760f37abb639aad49ef11bb1d37923fade7a62c9
Himel Das <thehimelofficial@gmail.com>
2021-10-09 13:58:31 +0200

    updated cleaning-schedule

--- src/cleaning-schedule/run.py
+++ src/cleaning-schedule/run.py
@@ -12,29 +12,28 @@ def generate(persons, start_date, days, interval):
     """Generate the output."""
 
     data = []
-
     start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y").date()
     end_date = start_date + datetime.timedelta(days)  # Find the end date.
-
-    # text = "Cleaning Schedule\n"
-
-    # Table header
-    data.append(['Date', 'Name', 'Kitchen', 'WC'])
-
+    data.append(["Date", "Bin", "Kitchen and WC"])  # Table header
     present_date = start_date
-    index = 0
+    bin_index = kwc_index = count = 0
+
     while present_date <= end_date:
         date = format_date(present_date)
-        person = persons[index]
-        data.append([date, person, '[  ]', '[  ]'])
-
-        # Add the interval to the present date.
+        bin_person = persons[bin_index]
+        if count % 2 == 0:
+            kitchen = f"{persons[kwc_index]} [  ]"
+            kwc_index += 1
+            if kwc_index > len(persons) - 1:
+                kwc_index = 0
+        else:
+            kitchen = ""
+        data.append([date, f"{bin_person} [  ]", kitchen])
         present_date += datetime.timedelta(interval)
-
-        # Change the person.
-        index += 1
-        if index > len(persons) - 1:
-            index = 0
+        bin_index += 1
+        if bin_index > len(persons) - 1:
+            bin_index = 0
+        count += 1
 
     return data
 
@@ -66,23 +65,22 @@ def generate_pdf(data):
     spacing = 2
 
     pdf.set_font("Arial", size=16)
-    pdf.cell(w=0, h=20, txt='Cleaning Schedule', ln=1, align='C')
+    pdf.cell(w=0, h=20, txt="Cleaning Schedule", ln=1, align="C")
 
     pdf.set_font("Arial", size=12)
     for row in data:
         for item in row:
-            pdf.cell(w=col_width, h=row_height*spacing,
-                     txt=item, border=1, align='C')
-        pdf.ln(row_height*spacing)
+            pdf.cell(w=col_width, h=row_height * spacing, txt=item, border=1, align="C")
+        pdf.ln(row_height * spacing)
 
-    pdf.output('clean.pdf')
+    pdf.output("clean.pdf")
 
 
 if __name__ == "__main__":
     # generate(*take_input())
-    persons = ['Smith', 'Ben']
-    start_date = '09-05-2021'
-    days = 120
+    persons = ["Smith", "Ben"]
+    start_date = "10-10-2021"
+    days = 180
     interval = 7
     test_data = (persons, start_date, days, interval)
     data = generate(*test_data)
