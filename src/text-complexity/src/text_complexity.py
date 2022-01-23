b55ab0a9aec37071c7de149f2f2769b71c4b089a
Himel Das <thehimelofficial@gmail.com>
2022-01-23 22:39:45 +0100

    Update: text-complexity * Updated cproject structure.

--- src/text-complexity/src/text_complexity.py
+++ src/text-complexity/src/text_complexity.py
@@ -5,5 +5,5 @@ Analyze the complexity of texts.
 from read import read
 
 if __name__ == "__main__":
-    res = read(file_path="text.txt")
+    res = read(file_path="files/text.txt")
     print(res)
