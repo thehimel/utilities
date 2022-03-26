ccf5e0d1718a7be7ce13621472ebf546f95473dc
Himel Das <thehimelofficial@gmail.com>
2022-03-26 03:33:19 +0100

    update: alerts * Updated main function.

--- alerts/src/alerts.py
+++ alerts/src/alerts.py
@@ -22,22 +22,21 @@ def audio(text: str, title: str) -> AudioPlayer:
     return AudioPlayer(audio_path)
 
 
-TONE = "Ten tena!" * 3
-audio_drink_water = audio(text=f"{TONE} Please drink water!", title="drink_water")
-audio_eat_fruits = audio(text=f"{TONE} Please eat fruits!", title="eat_fruits")
-
-
-def drink_water():
+def drink_water() -> None:
     """Play audio to drink water."""
     audio_drink_water.play(block=False)
 
 
-def eat_fruits():
+def eat_fruits() -> None:
     """Play audio to eat fruits."""
     audio_eat_fruits.play(block=False)
 
 
 if __name__ == "__main__":
+    TONE = "Ten tena!" * 3
+    audio_drink_water = audio(text=f"{TONE} Please drink water!", title="drink_water")
+    audio_eat_fruits = audio(text=f"{TONE} Please eat fruits!", title="eat_fruits")
+
     try:
         scheduler = BlockingScheduler(timezone=pytz.timezone("Europe/Berlin"))
         scheduler.add_job(eat_fruits, "cron", hour=16, minute=0)  # Run this job everyday at 16:00 PM.
