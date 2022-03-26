542fe27e976e53f889de19f6752ab3d50b6d366b
Himel Das <thehimelofficial@gmail.com>
2022-03-26 14:24:20 +0100

    update: alerts * Dynamically add jobs from json file.
    update: utilities
    * Added Pylint MESSAGES CONTROL in pyproject.toml

--- alerts/README.md
+++ alerts/README.md
@@ -9,3 +9,6 @@
   * [apscheduler.triggers.cron](https://apscheduler.readthedocs.io/en/3.x/modules/triggers/cron.html)
   * [BlockingScheduler start immediately](https://stackoverflow.com/questions/43254531/python-apschedule-blockingscheduler-with-interval-trigger-start-immediately)
   * [pytz - World Timezone Definitions for Python](http://pytz.sourceforge.net/#country-information)
+  * [Logging in APScheduler](https://groups.google.com/g/apscheduler/c/EA91eKGl8tM)
+  * [Passing parameters to apscheduler handler function](https://stackoverflow.com/questions/12412708/passing-parameters-to-apscheduler-handler-function)
+* [Directory of the script being run](https://stackoverflow.com/questions/3430372/how-do-i-get-the-full-path-of-the-current-files-directory)
--- alerts/src/alerts.py
+++ alerts/src/alerts.py
@@ -2,6 +2,8 @@
 Plays alerts for given tasks.
 """
 
+import json
+import logging
 from pathlib import Path
 from datetime import datetime
 import pytz
@@ -10,36 +12,68 @@ from audioplayer import AudioPlayer
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s]: %(levelname)s : %(message)s')
+FILE_PATH = Path(__file__).parent.resolve()  # Path for the directory of this file.
+
+
 def audio(text: str, title: str) -> AudioPlayer:
     """Convert text to audio."""
     title = f"{title}.mp3" if not title.endswith(".mp3") else title
-    audio_path = Path("audio")
+    audio_path = Path(FILE_PATH / "audio")
     audio_path.mkdir(parents=True, exist_ok=True)  # Create directory if doesn't exists.
     audio_path = Path(audio_path / title).as_posix()
-    speech = gTTS(text=text, lang='en', slow=False)  # Due to slow=False, the converted audio should have a high speed.
+    speech = gTTS(text=text, lang="en", slow=False)  # Due to slow=False, the converted audio should have a high speed.
     speech.save(audio_path)
     return AudioPlayer(audio_path)
 
 
-def drink_water() -> None:
-    """Play audio to drink water."""
-    audio_drink_water.play(block=False)
+def play(audio_player: AudioPlayer, schedule: dict) -> None:
+    """Play the audio"""
+    logging.info(f"Playing: {schedule}")
+    audio_player.play(block=False)
 
 
-def eat_fruits() -> None:
-    """Play audio to eat fruits."""
-    audio_eat_fruits.play(block=False)
+def handle() -> None:
+    """Handler for the execution of jobs."""
+    scheduler = BlockingScheduler(timezone=pytz.timezone("Europe/Berlin"))
+    schedule_types = {"interval": "interval", "cron": "cron"}
+    tone = "Ten tena! " * 3
+    audio_files = {}
+
+    with open(Path(FILE_PATH / "schedules.json"), encoding="utf-8") as schedules_json:
+        schedules = json.load(schedules_json)
+
+    for schedule in schedules:
+        if schedule["type"].lower() == schedule_types["interval"]:
+            title = schedule["title"]
+            if title not in audio_files:
+                audio_files[title] = audio(text=f"{tone}{schedule['message']}", title=title)
+            scheduler.add_job(
+                play,  # function name
+                schedule_types["interval"],
+                [audio_files[title], schedule],  # function parameters
+                seconds=schedule["interval"],
+                next_run_time=datetime.now()
+            )
+        elif schedule["type"].lower() == schedule_types["cron"]:
+            title = schedule["title"]
+            if title not in audio_files:
+                audio_files[title] = audio(text=f"{tone}{schedule['message']}", title=title)
+            # Run this job everyday at a specific time.
+            scheduler.add_job(
+                play,
+                schedule_types["cron"],
+                [audio_files[title], schedule],
+                hour=schedule["time"]["hour"],
+                minute=schedule["time"]["minute"]
+            )
+        else:
+            logging.error(f"Invalid schedule type: {schedule['type']} for schedule: {schedule}")
+    scheduler.start()
 
 
 if __name__ == "__main__":
-    TONE = "Ten tena!" * 3
-    audio_drink_water = audio(text=f"{TONE} Please drink water!", title="drink_water")
-    audio_eat_fruits = audio(text=f"{TONE} Please eat fruits!", title="eat_fruits")
-
     try:
-        scheduler = BlockingScheduler(timezone=pytz.timezone("Europe/Berlin"))
-        scheduler.add_job(eat_fruits, "cron", hour=16, minute=0)  # Run this job everyday at 16:00 PM.
-        scheduler.add_job(drink_water, "interval", seconds=3600, next_run_time=datetime.now())
-        scheduler.start()
+        handle()
     except KeyboardInterrupt:
-        print("Stay healthy!")
+        logging.info("Stay healthy!")
